54e84adf6acc1c8c1d9cdc9a7e28ec27996c0bd9
ejcontrerasv <ejcontrerasv@gmail.com>
2021-04-25 05:31:39 -0400

    Initial commit

---
+++ src/test/java/com/bandido/app/zuul/ZuulServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bandido.app.zuul;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
